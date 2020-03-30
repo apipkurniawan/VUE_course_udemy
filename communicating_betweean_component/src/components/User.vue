d8f23ffb9c297f5c0185f743eacd8d33783a2daf
apip <apip@gitlab.lab>
2020-03-30 21:34:46 +0700

    communicating between component

--- communicating_betweean_component/src/components/User.vue
+++ communicating_betweean_component/src/components/User.vue
@@ -1,33 +1,55 @@
 <template>
-    <div class="component">
-        <h1>The User Component</h1>
-        <p>I'm an awesome User!</p>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12 col-sm-6">
-                <app-user-detail></app-user-detail>
-            </div>
-            <div class="col-xs-12 col-sm-6">
-                <app-user-edit></app-user-edit>
-            </div>
-        </div>
+  <div class="component">
+    <h1>The User Component</h1>
+    <p>I'm an awesome User!</p>
+    <button @click="changeName">Change My Name</button>
+    <p>Name is {{ name }}</p>
+    <p>Age is {{ age }}</p>
+    <hr />
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <app-user-detail
+          :myName="name"
+          :resetFn="resetName"
+          :userAge="age"
+          @nameWasReset="name = $event"
+        ></app-user-detail>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <app-user-edit :userAge="age" @ageWasEdited="age = $event"></app-user-edit>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import UserDetail from './UserDetail.vue';
-    import UserEdit from './UserEdit.vue';
+import UserDetail from "./UserDetail.vue";
+import UserEdit from "./UserEdit.vue";
 
-    export default {
-        components: {
-            appUserDetail: UserDetail,
-            appUserEdit: UserEdit
-        }
+export default {
+  components: {
+    appUserDetail: UserDetail,
+    appUserEdit: UserEdit
+  },
+  data: function() {
+    return {
+      name: "Max",
+      age: 27
+    };
+  },
+  methods: {
+    changeName: function() {
+      this.name = "apip";
+    },
+    resetName: function() {
+      this.name = "max";
     }
+  }
+};
 </script>
 
 <style scoped>
-    div {
-        background-color: lightblue;
-    }
+div {
+  background-color: lightblue;
+}
 </style>
--- communicating_betweean_component/src/components/UserDetail.vue
+++ communicating_betweean_component/src/components/UserDetail.vue
@@ -1,15 +1,49 @@
 <template>
-    <div class="component">
-        <h3>You may view the User Details here</h3>
-        <p>Many Details</p>
-    </div>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <p>User Name : {{ switchName() }}</p>
+    <p>User Age : {{ userAge }}</p>
+    <button @click="resetName">Reset Name</button>
+    <button @click="resetFn()">Reset Name Callback</button>
+  </div>
 </template>
 
 <script>
+import { eventBus } from "../main";
+export default {
+  // props: ["myName"],
+  /* using validate props */
+  props: {
+    myName: {
+      type: String
+    },
+    /* using callback function */
+    resetFn: Function,
+    userAge: Number
+  },
+  methods: {
+    switchName: function() {
+      return this.myName
+        .split("")
+        .reverse()
+        .join("");
+    },
+    resetName: function() {
+      this.myName = "Max";
+      this.$emit("nameWasReset", this.myName);
+    }
+  },
+  created() {
+    eventBus.$on("ageWasEdited", age => {
+      this.userAge = age;
+    });
+  }
+};
 </script>
 
 <style scoped>
-    div {
-        background-color: lightcoral;
-    }
+div {
+  background-color: lightcoral;
+}
 </style>
