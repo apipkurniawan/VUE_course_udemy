14a535d359d3511b12c448e3e546a56b8a7627a1
apip <apip@gitlab.lab>
2020-03-31 23:49:41 +0700

    creating wonderful quotes

---
+++ wonderful_quote/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <app-new-quote @quoteAdded="createNewQuote"></app-new-quote>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info : Click on a Quote to Delete it!</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import QuoteGrid from "./components/QuoteGrid.vue";
+import NewQuote from "./components/NewQuote.vue";
+import Header from "./components/Header.vue";
+
+export default {
+  data() {
+    return {
+      quotes: ["Just a quote to see something"],
+      maxQuotes: 10
+    };
+  },
+  components: {
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appHeader: Header
+  },
+  methods: {
+    createNewQuote(quotes) {
+      if (this.quotes.length >= 10) {
+        alert("Please Delete the Quote!");
+      } else {
+        this.quotes.push(quotes);
+      }
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
