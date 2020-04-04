511704b8a498bd4945c5b71a47d2fde3cea636c6
apip <apip@gitlab.lab>
2020-04-04 09:39:07 +0700

    learn how to connect to server

---
+++ connect_to_servers/src/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Http</h1>
+        <div class="form-group">
+          <label>Username</label>
+          <input type="text" class="form-control" v-model="user.username" />
+        </div>
+        <div class="form-group">
+          <label>Mail</label>
+          <input type="text" class="form-control" v-model="user.email" />
+        </div>
+        <button class="btn btn-primary" @click="submit">Submit</button>
+        <hr />
+        <input type="text" v-model="node" class="form-control" />
+        <br />
+        <br />
+        <button class="btn btn-primary" @click="fetchData">Get Data</button>
+        <br />
+        <br />
+        <ul class="list-group">
+          <li class="list-group-item" v-for="u in users" :key="u">{{ u.username }} - {{ u.email }}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        username: "",
+        email: ""
+      },
+      users: [],
+      resource: {},
+      node: "data"
+    };
+  },
+  methods: {
+    submit() {
+      this.resource.saveAlt(this.user).then(res => {
+        if (res.ok == true) {
+          alert("berhasil simpan!");
+          this.user.username = "";
+          this.user.email = "";
+        } else {
+          alert(res.statusText);
+        }
+      });
+    },
+    fetchData() {
+      // this.$http
+      //   .get("data.json")
+      //   .then(response => {
+      //     return response.json();
+      //   })
+      //   .then(data => {
+      //     const resultArray = [];
+      //     for (let key in data) {
+      //       resultArray.push(data[key]);
+      //     }
+      //     this.users = resultArray;
+      //   });
+      this.resource
+        .getData({ node: this.node })
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          const resultArray = [];
+          for (let key in data) {
+            resultArray.push(data[key]);
+          }
+          this.users = resultArray;
+        });
+    }
+  },
+  created() {
+    const customAction = {
+      saveAlt: { method: "POST", url: "alternative.json" },
+      getData: { method: "GET" }
+    };
+    this.resource = this.$resource("{node}.json", {}, customAction);
+  }
+};
+</script>
+
+<style>
+</style>
